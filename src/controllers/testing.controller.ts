9d17bba21c615972b9a73b6863bb77c9b69bba23
Alexander Holub <holub.alexander.it@gmail.com>
2023-01-14 20:04:24 +0200

    feat: update /testing route

--- src/controllers/testing.controller.ts
+++ src/controllers/testing.controller.ts
@@ -4,10 +4,8 @@ import { blogsService } from "../services/blogs.service";
 import { postsService } from "../services/posts.service";
 
 export const deleteAllHandler = async (_: Request, res: Response) => {
-  const deletedBlogs = await blogsService.deleteAllBlogs();
-  const deletedPosts = await postsService.deleteAllPosts();
+  await blogsService.deleteAllBlogs();
+  await postsService.deleteAllPosts();
 
-  if (deletedBlogs && deletedPosts) {
-    return res.sendStatus(constants.HTTP_STATUS_NO_CONTENT);
-  }
+  res.sendStatus(constants.HTTP_STATUS_NO_CONTENT);
 };
