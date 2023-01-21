346f1a18886ea371c4149f9ba8bbd2720d8f6462
Alexander Holub <holub.alexander.it@gmail.com>
2023-01-22 01:24:44 +0200

    test: changes made to the POST /:blogId/posts method

--- src/utils/common/getObjectToSort.ts
+++ src/utils/common/getObjectToSort.ts
@@ -1,7 +1,7 @@
 import { SortDirections, SortQueryParams } from "../../@types";
 
 export const getObjectToSort = ({ sortBy, sortDirection }: SortQueryParams) => {
-  const sortDirectionValue = sortDirection === SortDirections.ASC ? SortDirections.ASC : SortDirections.DESC;
+  const sortDirectionValue = sortDirection !== SortDirections.ASC ? SortDirections.ASC : SortDirections.DESC;
 
   if (sortBy) {
     return { [sortBy]: sortDirectionValue };
